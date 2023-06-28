628415755ff449579afcb5035c3a3643b2a75193
Gen Watanabe <gwatanab@adobe.com>
2023-06-28 17:42:33 +0900

    Add blog-list and academy.

--- scripts/utils.js
+++ scripts/utils.js
@@ -37,6 +37,20 @@ export const [setLibs, getLibs] = (() => {
  * ------------------------------------------------------------
  */
 
+export function initSidekick() {
+  const initPlugins = async () => {
+    const { default: init } = await import('./sidekick.js');
+    init();
+  };
+  if (document.querySelector('helix-sidekick')) {
+    initPlugins();
+  } else {
+    document.addEventListener('sidekick-ready', () => {
+      initPlugins();
+    });
+  }
+}
+
 export async function useMiloSample() {
   const { createTag } = await import(`${getLibs()}/utils/utils.js`);
 }
